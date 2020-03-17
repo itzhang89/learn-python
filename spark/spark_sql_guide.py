a828f64c83e6bd01cf5643877e8b9f1471ada4cc
jinghui.zhang <jinghui.zhang@thoughtworks.com>
2020-03-17 15:55:38 +0800

    add a demo for init spark sql

---
+++ spark/spark_sql_guide.py
@@ -0,0 +1,20 @@
+from pyspark.sql import SparkSession
+
+# Starting Point: SparkSession
+spark = SparkSession \
+    .builder \
+    .appName("Python Spark SQL basic example") \
+    .config("spark.some.config.option", "some-value") \
+    .getOrCreate()
+
+# spark is an existing SparkSession
+df = spark.read.json("examples/src/main/resources/people.json")
+# Displays the content of the DataFrame to stdout
+df.show()
+# +----+-------+
+# | age|   name|
+# +----+-------+
+# |null|Michael|
+# |  30|   Andy|
+# |  19| Justin|
+# +----+-------+
