b62dc100c8c39ac99e0c3c1d364771a629ea0da7
jinghui.zhang <jinghui.zhang@thoughtworks.com>
2020-03-24 13:39:34 +0800

    add a simple for dataframe in pyspark

---
+++ spark/dataframe/01_Starting_Point.py
@@ -0,0 +1,68 @@
+from pyspark.sql import SparkSession
+
+spark = SparkSession \
+    .builder \
+    .appName("Python Spark SQL basic example") \
+    .config("spark.some.config.option", "some-value") \
+    .getOrCreate()
+
+# spark is an existing SparkSession
+data = [(None, "Michael"), (30, "Andy"), (19, "Justin")]
+df = spark.createDataFrame(data, schema=["age", "name"])
+# df = spark.read.json("spark/resources/person.json")
+# Displays the content of the DataFrame to stdout
+df.show()
+# +----+-------+
+# | age|   name|
+# +----+-------+
+# |null|Michael|
+# |  30|   Andy|
+# |  19| Justin|
+# +----+-------+
+
+# Untyped Dataset Operations (aka DataFrame Operations)
+
+# spark, df are from the previous example
+# Print the schema in a tree format
+df.printSchema()
+# root
+# |-- age: long (nullable = true)
+# |-- name: string (nullable = true)
+
+# Select only the "name" column
+df.select("name").show()
+# +-------+
+# |   name|
+# +-------+
+# |Michael|
+# |   Andy|
+# | Justin|
+# +-------+
+
+# Select everybody, but increment the age by 1
+df.select(df['name'], df['age'] + 1).show()
+# +-------+---------+
+# |   name|(age + 1)|
+# +-------+---------+
+# |Michael|     null|
+# |   Andy|       31|
+# | Justin|       20|
+# +-------+---------+
+
+# Select people older than 21
+df.filter(df['age'] > 21).show()
+# +---+----+
+# |age|name|
+# +---+----+
+# | 30|Andy|
+# +---+----+
+
+# Count people by age
+df.groupBy("age").count().show()
+# +----+-----+
+# | age|count|
+# +----+-----+
+# |  19|    1|
+# |null|    1|
+# |  30|    1|
+# +----+-----+
---
+++ spark/dataframe/02_Running_SQL_Programmatically.py
@@ -0,0 +1,34 @@
+from pyspark.sql import SparkSession
+
+spark = SparkSession \
+    .builder \
+    .appName("Python Spark SQL basic example") \
+    .config("spark.some.config.option", "some-value") \
+    .getOrCreate()
+
+# spark is an existing SparkSession
+data = [(None, "Michael"), (30, "Andy"), (19, "Justin")]
+df = spark.createDataFrame(data, schema=["age", "name"])
+
+# Register the DataFrame as a global temporary view
+df.createGlobalTempView("people")
+
+# Global temporary view is tied to a system preserved database `global_temp`
+spark.sql("SELECT * FROM global_temp.people").show()
+# +----+-------+
+# | age|   name|
+# +----+-------+
+# |null|Michael|
+# |  30|   Andy|
+# |  19| Justin|
+# +----+-------+
+
+# Global temporary view is cross-session
+spark.newSession().sql("SELECT * FROM global_temp.people").show()
+# +----+-------+
+# | age|   name|
+# +----+-------+
+# |null|Michael|
+# |  30|   Andy|
+# |  19| Justin|
+# +----+-------+
