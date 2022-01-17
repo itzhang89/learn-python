7debd9625672ae1cb7ba208f0b84515bc756b51c
Jinghui Zhang <30542109+yutianaiqingtian@users.noreply.github.com>
2022-01-17 15:31:53 +0800

    format the ddl table and filter nan field

--- pandas/main.py
+++ pandas/main.py
@@ -24,6 +24,22 @@ def create_table_ddl_fs(db_name: str, table_name, fileds: list):
         fileds_str)
 
 
+def table_name_fm(sys_name: str, table_name: str, pre_ods: bool = False, db_name: str = None, schema_name: str = None):
+    items = []
+    items.append("pre_ods" if pre_ods else "ods")
+    items.append(sys_name)
+    if db_name:
+        items.append(db_name)
+    if schema_name:
+        items.append(schema_name)
+    items.append(table_name)
+    full_table_name = "__".join(items)
+
+    if full_table_name.find('$') != -1:
+        full_table_name = '`' + full_table_name + '`'
+    return full_table_name
+
+
 def is_empty(field) -> bool:
     if field is None:
         return True
@@ -31,6 +47,8 @@ def is_empty(field) -> bool:
     if len(field) == 0:
         return True
     if field == 'null':
+        return True
+    if field == 'nan':
         return True
     return False
 
@@ -44,13 +62,15 @@ def get_field_column_definition(series) -> str:
         return ''
 
     # generate data type
-    column_type = str(series[DATA_TYPE]).strip()
-    column_length = str(series[DATA_LENGTH]).strip()
-    precision = str(series[DATA_PRECISION]).strip()
-    if is_empty(precision):
-        column_type = "{}({},{})".format(column_type, column_length, precision)
+    column_type = series[DATA_TYPE]
+    column_length = series[DATA_LENGTH]
+    precision = series[DATA_PRECISION]
+    if is_empty(column_length):
+        column_type = column_type
+    elif is_empty(precision):
+        column_type = "{}({})".format(column_type, round(column_length))
     else:
-        column_type = "{}({})".format(column_type, column_length)
+        column_type = "{}({},{})".format(column_type, round(column_length), round(precision))
 
     is_nullable = "NOT NULL" if series[NULLABLE] else ""
 
@@ -65,9 +85,21 @@ if __name__ == '__main__':
     df: DataFrame = pd.read_csv(xlsx, header=0).drop(['CHARACTER_SET_NAME'], axis=1)
     df[NULLABLE] = df[NULLABLE].map(isNullable).astype(bool)
 
-    print(df.info())
+    #  select 21 table name
+    selected_tables = ['FORECAST_SUMMARY', 'allotment$detail', 'Memberships', 'name_view', 'reservation_items',
+                       'RESERVATION_DAILY_ELEMENT_NAME', 'RESERVATION_NAME', 'reservation_products', 'trx_routing_instructions',
+                       'RESERVATION_SUMMARY', 'EXTERNAL_REFERENCES', 'RESERVATION_SUMMARY', 'FORECAST_SUMMARY',
+                       'allotment$header', 'NAME', 'RESERVATION_DAILY_ELEMENTS', 'RESERVATION_DAILY_ELEMENT_NAME',
+                       'RESERVATION_NAME', 'NAME_PHONE', 'RESERVATION_COMMENT', 'name_address', 'postal_codes_chain',
+                       'name_address', 'postal_codes_chain']
+
+    selected_tables = list(map(str.upper, selected_tables))
+    print(selected_tables)
+
+    df = df[df[TABLE_NAME].isin(selected_tables)]
     sql_df_groupby: DataFrameGroupBy = df.groupby(TABLE_NAME)
 
     for db_name, sub_df in sql_df_groupby:
         pd_column = sub_df.apply(get_field_column_definition, axis=1)
-        print(create_table_ddl_fs("melco_opera", db_name, np.array(pd_column)))
+        print(create_table_ddl_fs("melco_opera", table_name_fm(sys_name='opera', db_name='operastaging', table_name=db_name),
+                                  np.array(pd_column)))
