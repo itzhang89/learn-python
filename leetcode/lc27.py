f36848cf14dcc276bf7da5ad0a0eaa5faace1052
itzhang89 <30542109+yutianaiqingtian@users.noreply.github.com>
2022-03-07 23:02:45 +0800

    leetcode 27 method2

--- leetcode/lc27.py
+++ leetcode/lc27.py
@@ -2,7 +2,7 @@ from typing import List
 
 
 class Solution:
-    def removeElement(self, nums: List[int], val: int) -> int:
+    def removeElement1(self, nums: List[int], val: int) -> int:
         i, j = 0, len(nums) - 1
         while i <= j:
             if nums[j] == val:
@@ -14,13 +14,28 @@ class Solution:
             i += 1
         return min(i, j) + 1
 
+    def removeElement2(self, nums: List[int], val: int) -> int:
+        slow, fast = 0, 0
+        while fast < len(nums):
+            if nums[fast] != val:
+                nums[slow] = nums[fast]
+                slow += 1
+            fast += 1
+        return slow
+
 
 if __name__ == '__main__':
     nums = [3, 2, 2, 3]
-    assert Solution().removeElement(nums, 3) == 2
+    assert Solution().removeElement1(nums, 3) == 2
+    nums = [3, 2, 2, 3]
+    assert Solution().removeElement2(nums, 3) == 2
 
     nums = [0, 1, 2, 2, 3, 0, 4, 2]
-    assert Solution().removeElement(nums, 2) == 5
+    assert Solution().removeElement1(nums, 2) == 5
+    nums = [0, 1, 2, 2, 3, 0, 4, 2]
+    assert Solution().removeElement2(nums, 2) == 5
 
     nums = [1]
-    assert Solution().removeElement(nums, 1) == 0
+    assert Solution().removeElement1(nums, 1) == 0
+    nums = [1]
+    assert Solution().removeElement2(nums, 1) == 0
